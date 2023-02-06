91be448ca8f0f9e4457daa55424337da95f06a3d
Anitap <123569163+Anitapppp@users.noreply.github.com>
2023-02-06 12:02:04 -0500

    Cambios(06/03/23

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+const cardsAdj =[
+  {
+    name: 'Alpha Turi'
+    img: 'imagenes/alpha turi.jpg'
+  },
+  {
+    name: 'Aston Martin'
+    img: 'imagenes/aston martin.jpg'
+  },
+  {
+    name: 'Ferrari'
+    img: 'imagenes/ferrari.jpg'
+  },
+  {
+    name: 'Haas'
+    img: 'imagenes/haas.jpg'
+  },
+  {
+    name: 'Mclaren'
+    img: 'imagenes/mclaren.jpg'
+  },
+  {
+    name: 'Mercedes'
+    img: 'imagenes/mercedes.jpg'
+  },
+
+]
+});
