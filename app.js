3cb2d98f0eb772eb1994ae1a25616854a1bb412d
Anitap <123569163+Anitapppp@users.noreply.github.com>
2023-02-08 10:20:28 -0500

    cambios(08/02/23)

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-const cardsAdj =[
-  {
-    name: 'Alpha Turi'
-    img: 'imagenes/alpha turi.jpg'
-  },
-  {
-    name: 'Aston Martin'
-    img: 'imagenes/aston martin.jpg'
-  },
-  {
-    name: 'Ferrari'
-    img: 'imagenes/ferrari.jpg'
-  },
-  {
-    name: 'Haas'
-    img: 'imagenes/haas.jpg'
-  },
-  {
-    name: 'Mclaren'
-    img: 'imagenes/mclaren.jpg'
-  },
-  {
-    name: 'Mercedes'
-    img: 'imagenes/mercedes.jpg'
-  },
-  
-
-]
+  const cardAdj = [
+    {
+      name: "Alpha Turi",
+      img: "imagenes/alpha turi.jpg"
+    },
+    {
+      name: "Alpha Turi",
+      img: "imagenes/alpha turi.jpg"
+    },
+    {
+      name: "Aston Martin",
+      img: "imagenes/aston martin.jpg"
+    },
+    {
+      name: "Aston Martin",
+      img: "imagenes/aston martin.jpg"
+    },
+    {
+      name: "Ferrari",
+      img: "imagenes/ferrari.jpg"
+    },
+    {
+      name: "Ferrari",
+      img: "imagenes/ferrari.jpg"
+    },
+    {
+      name: "Haas",
+      img: "imagenes/haas.jpg"
+    },
+    {
+      name: "Haas",
+      img: "imagenes/haas.jpg"
+    },
+    {
+      name: "Mclaren",
+      img: "imagenes/mclaren.jpg"
+    },
+    {
+      name: "Mclaren",
+      img: "imagenes/mclaren.jpg"
+    },
+    {
+      name: "Mercedes",
+      img: "imagenes/mercedes.jpg"
+    },
+    {
+      name: "Mercedes",
+      img: "imagenes/mercedes.jpg"
+    }
+  ];
 });
